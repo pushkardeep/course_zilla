5bc785a8bd207a1fbe746de60af9900044617173
Pushkardeep <just.pushkardeep@gmail.com>
2024-08-14 12:31:35 +0530

    commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,15 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      injectRegister: "auto", // This ensures that the service worker registration happens automatically
+      workbox: false, // Disable Workbox since you're using a custom service worker
+      strategies: "generateSW", // Ensure VitePWA does not generate another service worker
+      devOptions: {
+        enabled: true, // Enable for development, but ensure to use your own sw.js in production
+      },
+    }),
+  ],
 });
